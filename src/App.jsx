58a3525310f3f6098a44c4a92d8f1c3cd195ae1d
MaxiHerrera97 <maximilianogabriel0511@gmail.com>
2023-12-22 20:52:26 -0300

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,36 +8,15 @@ import NavBar from "./components/NavBar/NavBar";
 
 
 
+
+
 function App() {
   
 
   return (
     <>
-    <div class="bubbles">
-        <span styles="-i:11"></span>
-        <span styles="-i:12"></span>
-        <span styles="-i:24"></span>
-        <span styles="-i:10"></span>
-        <span styles="-i:14"></span>
-        <span styles="-i:23"></span>
-        <span styles="-i:18"></span>
-        <span styles="-i:16"></span>
-        <span styles="-i:19"></span>
-        <span styles="-i:20"></span>
-        <span styles="-i:22"></span>
-        <span styles="-i:25"></span>
-        <span styles="-i:18"></span>
-        <span styles="-i:21"></span>
-        <span styles="-i:15"></span>
-        <span styles="-i:13"></span>
-        <span styles="-i:26"></span>
-        <span styles="-i:17"></span>
-        <span styles="-i:13"></span>
-        <span styles="-i:28"></span>
-        </div>
+    
       <div class="container">
-
-        
         <NavBar/>
         <Routes>
           <Route path="/" exact Component={Home}/>
